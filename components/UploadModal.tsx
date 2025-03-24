e14df6efeb559e062468ee0d347e27f793c0c03c
mashtheking <fredmaina174@gmail.com>
2025-03-24 17:54:40 +0300

    Update UploadModal.tsx

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -71,7 +71,7 @@ export const UploadModal = () => {
 
       //* Upload image to Supabase storage
       const { data: imageData, error: imageError } = await supabaseClient.storage
-        .from('images')
+        .from('public/images')
         .upload(`image-${values.title}-${uniqueID}`, imageFile, {
           cacheControl: '3600',
           upsert: false,
@@ -83,7 +83,7 @@ export const UploadModal = () => {
       }
 
       //* Insert new song record in the Supabase 'songs' table
-      const { error: supabaseError } = await supabaseClient.from('songs').insert({
+      const { error: supabaseError } = await supabaseClient.from('public/songs').insert({
         user_id: user.id,
         title: values.title,
         author: values.author,
