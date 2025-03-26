c447fc0cf4cdab486fe9201bdbf01ee58d7d6ad5
mashtheking <fredmaina174@gmail.com>
2025-03-26 14:46:22 +0300

    Update useOnPlay.ts

--- hooks/useOnPlay.ts
+++ hooks/useOnPlay.ts
@@ -9,7 +9,8 @@ import { useGetSongById} from './useGetSongById';
 
 export const useOnPlay = (songs: Song[]) => {
   const supabaseClient = useSupabaseClient();
-        const [songD, setSong] = useState<Song> ;
+        const [songD, setSong] = useState<Song[]>([
+    [0, 0, 0, 0, 0, 0]) ;
 
 
 
